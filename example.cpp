4505fe41113ab4a8e8b89f4e22936d702f1fed67
wenke1020 <wenkzhao@fudan.edu.cn>
2018-05-15 22:14:59 +0800

    Create example.cpp

---
+++ example.cpp
@@ -0,0 +1,16 @@
+#include <string>
+
+namespace example
+{
+
+#include  "raft/main.cpp"
+#include  "paxos/main.cpp"
+#include  "pow/main.cpp"
+#include  "pos/main.cpp"
+#include  "dpos/main.cpp"
+#include  "bftDpos/main.cpp"
+#include  "bftScale/main.cpp"
+
+// to be added for future
+
+}
